bc1f5826cc60d9ed73ad6b904ec48d05b5957cb8
jaden <jadenbusiness@hotmail.com>
2021-12-03 03:14:49 +0000

    added index.js plus refactoring breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require('request');
-const search = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${search}`, (error, response, body) => {
-  if (error) {
-    console.log("Error: " + error);
-  }
-  //console.log(response && response.statusCode);
-  const data = JSON.parse(body);
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log("Breed Not Found!");
-  }
-});
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("Breed Not Found!");
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
